c4af5581d5e06c464002f49e2ffac1c5dd1c9cb6
Daniel Reis <Daniel Reis@Daniel>
2016-10-27 16:19:02 -0200

    Test

---
+++ SistemaChamadosNovo/src/SistemaChamadosMain.java
@@ -0,0 +1,15 @@
+import Persistencia.ChamadoDAO;
+
+public class SistemaChamadosMain {
+	
+	public static void main(String[] args){
+		
+		ChamadoDAO c = new ChamadoDAO();
+                
+                System.out.println(c.getChamados());
+                
+                
+                
+	}
+	
+}
