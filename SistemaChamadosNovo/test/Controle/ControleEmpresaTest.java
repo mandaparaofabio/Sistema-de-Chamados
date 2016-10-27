5e49b6a5d349d0d06712fcf0295d4a302b2a4bd6
Daniel Reis <Daniel Reis@Daniel>
2016-10-27 16:58:05 -0200

    Test

---
+++ SistemaChamadosNovo/test/Controle/ControleEmpresaTest.java
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+package Controle;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Daniel Reis 
+ */
+public class ControleEmpresaTest {
+    
+    
+    
+}
